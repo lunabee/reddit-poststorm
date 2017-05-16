fcc5f2ee2dea1faa803cbcd90f9f1ea64e001ee8
Luna Hackman-Bennett <contact@lunabennett.com>
2017-05-16 15:19:34 -0400

    Well, it's working if I remove the new PostDisplayComponent. Also divided contents of main.ts out into app.module.ts where it belongs.

---
+++ src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule, Component } from '@angular/core';
+import { PostDisplayComponent } from './components/APIService/post-display.component'
+
+
+@Component({
+  selector: 'my-app',
+  template: '<h1>I hate the homeless...</h1><span>ness problem that plagues this city. Everyone should have the basic... You know what, I don\'t need tiny cue cards!</span>'
+})
+export class AppComponent { }
+
+/*@NgModule({
+  imports: [ BrowserModule ],
+  declarations: [ PostDisplayComponent ],
+  bootstrap: [ PostDisplayComponent ],
+})
+class PostModule {}*/
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,12 @@
 import { NgModule, Component } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { PostDisplayComponent } from './app/components/APIService/post-display.component'
-
-
-@Component({
-  selector: 'my-app',
-  template: '<h1>I hate the homeless...</h1><span>ness problem that plagues this city. Everyone should have the basic... You know what, I don\'t need tiny cue cards!</span>'
-})
-class AppComponent { }
+import { AppComponent } from './app/app.module'
 
 @NgModule({
-  imports: [ BrowserModule, PostDisplayComponent ],
+  imports: [ BrowserModule ],
   declarations: [ AppComponent ],
-  bootstrap: [ AppComponent, PostDisplayComponent ]
+  bootstrap: [ AppComponent ]
 })
 class AppModule {}
 
