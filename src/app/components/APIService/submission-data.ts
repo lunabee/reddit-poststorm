e6725e37cd7be98c6c1bc4f48459330e294b57b2
Luna Hackman-Bennett <contact@lunabennett.com>
2017-05-17 12:55:02 -0400

    More mock data to play with, added a submission_hint element to introduce conditionally displaying elements in template. Also fixed package.json because I never actually npm init'd, whoops.

--- src/app/components/APIService/submission-data.ts
+++ src/app/components/APIService/submission-data.ts
@@ -3,8 +3,9 @@ export class SubmissionData {
  subreddit_name_prefixed: string;
  title: string;
  thumbnail: string;
- selftext_html: string;
- permalink: string;
+ selftext_html: string; //will be tricky to escape properly. Maybe should grab raw? but then formatting is shot
+ permalink: string; //this will need to be concat with www.reddit.com
  url: string;
  score: number;
+ submission_hint: string; //equivilant to reddit's post_hint, want consistent naming within app
 }
