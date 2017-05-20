9c082dd87e3409acba390c492919dc3f698b814d
contact@lunabennett.com <contact@lunabennett.com>
2017-05-19 21:58:43 -0400

    Scaffolding for functions for getting JSON data from Reddit servers, Sass tweaks(by no means final design).

--- src/app/components/APIService/submission-data.service.ts
+++ src/app/components/APIService/submission-data.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
 import { SubmissionData } from './submission-data';
 import { Http } from '@angular/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable()
 export class SubmissionDataService {
 
+    constructor( private http: Http ) { };
+    
+    getSubredditSubmissions( subredditUrl: Array<string> ) {
+        return this.http.get('subredditUrl')
+                        .map( response => <SubmissionData[]>response.json().data );
+    }
+    
 }
--- src/app/components/APIService/subreddit-url-provider.service.ts
+++ src/app/components/APIService/subreddit-url-provider.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-// define some arrays of subreddit names
+// define some arrays of subreddit names. This can be stored in db later along with result of jsonRequestUrlProvider so we don't have to concat those every time.
 let FutureSubreddits = [
     "futurology", "TheFutureIsNow", "Automate", "artificial", "MachineLearning"
 ]
@@ -9,4 +9,16 @@ let ActuallyFunnySubreddits = [
     "talesfromtechsupport", "programminghorror", "ProgrammerHumor", "tifu", "techsupportmacgyver"
 ]
 
+//not sure if I'm going to keep this here or move it elsewhere. creates array of urls to $http.get for later
+function jsonRequestUrlProvider(multireddit: Array<string>, subredditSort: string, submissionNum: number) {
+    let jsonUrlArray : Array<string> = [];
+    for (let subreddit of multireddit) {
+        let jsonRequestUrl = 'https://www.reddit.com/r/' + subreddit + '/' + subredditSort + '.json?limit=' + submissionNum;
+        jsonUrlArray.push(jsonRequestUrl);
+    }
+    return jsonUrlArray;
+};
 
+//tests for this...
+//find multireddit.length, check against returned jsonUrlArray.length
+//heuristic string validation
