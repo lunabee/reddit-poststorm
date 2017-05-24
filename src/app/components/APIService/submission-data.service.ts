a91525d5b0ddca53d5ef355bbbb195a114fafa11
contact@lunabennett.com <contact@lunabennett.com>
2017-05-23 21:15:25 -0400

    It's alive! Added in EventEmitter to update display component for  submission data.

--- src/app/components/APIService/submission-data.service.ts
+++ src/app/components/APIService/submission-data.service.ts
@@ -1,5 +1,5 @@
-import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/forkJoin';
@@ -7,24 +7,37 @@ import { SubmissionData } from './submission-data';
 
 @Injectable()
 export class submissionDataService {
-
-    constructor( private http: Http ) { };
+    submissionDataChange: EventEmitter<SubmissionData[]> = new EventEmitter();
+    constructor( 
+        private http: Http
+     ) { };
     
     getSubredditSubmissions( subredditURLs: Array<string> ) {
-        console.log(subredditURLs);
         let observables: Array<Observable<Response>> = [];
         for(let subredditURL of subredditURLs) {
             observables.push(this.http.get(subredditURL).map(res => res.json()));
         };
-        console.log(observables);
         Observable.forkJoin(observables).subscribe(results => {
-            console.log(results);
             let submissions: Array<Response> = [];
             for(let result of results) {
                 submissions = submissions.concat(result["data"]["children"]);
             };
-            console.log(submissions);
-            return submissions;
+            let submissionDataArray: SubmissionData[] = [];
+            for (let submission of submissions){
+                let submissionData = new SubmissionData();
+                submissionData.title = submission["data"]["title"];
+                submissionData.author = submission["data"]["author"];
+                submissionData.subreddit_name_prefixed = submission["data"]["subreddit_name_prefixed"];
+                submissionData.title = submission["data"]["title"];
+                submissionData.thumbnail = submission["data"]["thumbnail"];
+                submissionData.selftext = submission["data"]["selftext"];
+                submissionData.permalink = submission["data"]["permalink"];
+                submissionData.url = submission["data"]["url"];
+                submissionData.score = submission["data"]["score"];
+                submissionData.submission_hint = submission["data"]["post_hint"];
+                submissionDataArray.push(submissionData);
+            };
+            this.submissionDataChange.emit(submissionDataArray);
         });
     }
     
--- src/app/components/APIService/submission-display.component.ts
+++ src/app/components/APIService/submission-display.component.ts
@@ -1,6 +1,7 @@
 import { NgModule, Component } from '@angular/core';
 import { SubmissionData } from './submission-data';
 import { MOCKDATAS } from './submission-data-mocks.component'
+import { submissionDataService } from './submission-data.service';
 
 @Component({
     selector: 'submission-display',
@@ -9,7 +10,12 @@ import { MOCKDATAS } from './submission-data-mocks.component'
 })
 export class submissionDisplayComponent { 
     SubmissionDatas: SubmissionData[];
+    constructor(private submissionDataService: submissionDataService) {}
     ngOnInit() {
-        this.SubmissionDatas = MOCKDATAS;
+        var that = this;
+        this.submissionDataService.submissionDataChange
+            .subscribe(function(datas: SubmissionData[]) {
+                that.SubmissionDatas = datas;
+            });
     }
 }
